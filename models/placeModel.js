a9fd538635bd20ad6f20a7f5c4686e11be85d6a8
Claudinei Bido <bido.c@live.com>
2020-08-06 11:21:16 +0200

    place model validation

--- models/placeModel.js
+++ models/placeModel.js
@@ -8,12 +8,33 @@ const placeSchema = new mongoose.Schema({
     type: String,
     required: [true, 'The product name is required'],
     unique: true,
+    trim: true,
   },
   description: {
     type: String,
     required: [true, 'The description is required'],
+    trim: true,
   },
   country: String,
+  continent: String,
+  type: String,
+  difficulty: String,
+  price: {
+    type: Number,
+    required: [true, 'The price is required'],
+  },
+  cover: {
+    type: Object,
+    default: {},
+  },
+  imgs: {
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const Place = mongoose.model('Place', placeSchema);
