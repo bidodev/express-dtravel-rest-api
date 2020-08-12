9733535db8482464260b0b131722b4632dd9680f
Claudinei Bido <bido.c@live.com>
2020-08-12 10:41:06 +0200

    user model validators

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@
  * Module dependencies.
  */
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 //name, email, photo, password, passwordConfirm
 
@@ -15,15 +16,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: ['true', 'The email is required'],
     unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please use a valid email'],
   },
   photo: {
     type: String,
   },
   password: {
     type: String,
+    required: ['true', 'Please provide a password'],
+    minlength: 10,
   },
   passwordConfirm: {
     type: String,
+    required: ['true', 'Please provide a password'],
   },
 });
 
