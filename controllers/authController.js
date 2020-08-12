95512d7052f494fdde38b3efe3827a6d82f0d076
Claudinei Bido <bido.c@live.com>
2020-08-12 15:07:26 +0200

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,17 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError.js');
 
 exports.signup = async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
